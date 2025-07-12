a0614ece5358f5df04627c1f75d21d47fac7cbb1
ChrisMathieu20 <mathieurchris@gmail.com>
2025-07-12 21:49:30 +0300

    Week02 - Temple Album

---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu');
+    const navigation = document.querySelector('.navigation');
+
+    menuButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        menuButton.classList.toggle('open');
+    });
+});
